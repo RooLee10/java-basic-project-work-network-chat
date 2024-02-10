5dc0f6043e9572b8f9b547148eb6b5b405736d7b
Vadim Li <Vadim@DESKTOP-EM6HT12.(none)>
2024-02-10 05:55:32 +0300

    init

---
+++ client/src/main/java/ru/li/chat/client/ClientApplication.java
@@ -0,0 +1,8 @@
+package ru.li.chat.client;
+
+public class ClientApplication {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ server/src/main/java/ru/li/chat/server/ServerApplication.java
@@ -0,0 +1,7 @@
+package ru.li.chat.server;
+
+public class ServerApplication {
+    public static void main(String[] args) {
+
+    }
+}
